b6c58f0e8b1a483ee3208092e8b1124ef08c58ce
a710010028 <710010028@qq.com>
2025-08-17 02:58:00 +0800

    Optimize the functionality of SAHTViewer

--- 3DS/UI/SAHTViewer.Designer.cs
+++ 3DS/UI/SAHTViewer.Designer.cs
@@ -28,101 +28,151 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SAHTViewer));
-			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-			this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-			this.listView1 = new LibEveryFileExplorer.UI.ListViewNF();
-			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.toolStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// toolStrip1
-			// 
-			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SAHTViewer));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.listView1 = new LibEveryFileExplorer.UI.ListViewNF();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
             this.toolStripButton2,
-            this.toolStripButton3});
-			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(487, 25);
-			this.toolStrip1.TabIndex = 0;
-			this.toolStrip1.Text = "toolStrip1";
-			this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
-			// 
-			// toolStripButton1
-			// 
-			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButton1.Name = "toolStripButton1";
-			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton1.Text = "toolStripButton1";
-			this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
-			// 
-			// toolStripButton2
-			// 
-			this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButton2.Enabled = false;
-			this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButton2.Name = "toolStripButton2";
-			this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton2.Text = "toolStripButton2";
-			this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
-			// 
-			// toolStripButton3
-			// 
-			this.toolStripButton3.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-			this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-			this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButton3.Name = "toolStripButton3";
-			this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton3.Text = "toolStripButton3";
-			// 
-			// listView1
-			// 
-			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5,
+            this.toolStripButton6,
+            this.toolStripButton7});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(487, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Add";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Enabled = false;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Delete";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
-			this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.listView1.GridLines = true;
-			this.listView1.LabelEdit = true;
-			this.listView1.Location = new System.Drawing.Point(0, 25);
-			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(487, 306);
-			this.listView1.TabIndex = 1;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
-			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-			// 
-			// columnHeader1
-			// 
-			this.columnHeader1.Text = "Name";
-			this.columnHeader1.Width = 400;
-			// 
-			// columnHeader2
-			// 
-			this.columnHeader2.Text = "Hash";
-			this.columnHeader2.Width = 100;
-			// 
-			// SAHTViewer
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(487, 331);
-			this.Controls.Add(this.listView1);
-			this.Controls.Add(this.toolStrip1);
-			this.Name = "SAHTViewer";
-			this.Text = "SAHTViewer";
-			this.Load += new System.EventHandler(this.SAHTViewer_Load);
-			this.toolStrip1.ResumeLayout(false);
-			this.toolStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(0, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(487, 281);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 400;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Hash";
+            this.columnHeader2.Width = 100;
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "Rename";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "Import Text";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "Export Text";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(123, 22);
+            this.toolStripButton7.Text = "Remove Duplicates";
+            this.toolStripButton7.Click += new System.EventHandler(this.toolStripButton7_Click);
+            // 
+            // SAHTViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 306);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "SAHTViewer";
+            this.Text = "SAHTViewer";
+            this.Load += new System.EventHandler(this.SAHTViewer_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -135,5 +185,9 @@
 		private System.Windows.Forms.ToolStripButton toolStripButton1;
 		private System.Windows.Forms.ToolStripButton toolStripButton2;
 		private System.Windows.Forms.ToolStripButton toolStripButton3;
-	}
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+    }
 }
--- 3DS/UI/SAHTViewer.cs
+++ 3DS/UI/SAHTViewer.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace _3DS.UI
@@ -18,16 +15,22 @@ namespace _3DS.UI
 			InitializeComponent();
 		}
 
-		private void SAHTViewer_Load(object sender, EventArgs e)
+        private bool isAscending = true;
+
+        private void SAHTViewer_Load(object sender, EventArgs e)
 		{
 			UpdateEntries();
-		}
+            toolStripButton3.Text = "Sort A-Z";
+            toolStripButton2.Enabled = false;
+            toolStripButton4.Enabled = false;
+        }
 
 		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			if (listView1.SelectedIndices.Count != 0) toolStripButton2.Enabled = true;
-			else toolStripButton2.Enabled = false;
-		}
+            bool hasSelection = listView1.SelectedIndices.Count > 0;
+            toolStripButton2.Enabled = hasSelection;
+            toolStripButton4.Enabled = hasSelection;
+        }
 
 		private void UpdateEntries()
 		{
@@ -53,24 +56,216 @@ namespace _3DS.UI
 			UpdateEntries();
 		}
 
-		private void toolStripButton2_Click(object sender, EventArgs e)
-		{
-			HashTable.Entries.RemoveAt(listView1.SelectedIndices[0]);
-			UpdateEntries();
-		}
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count == 0) return;
+            DialogResult result = 
+				MessageBox.Show($"The selected file name will be deleted, Do you want to continue?", "Warning",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2);
 
-		private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-		{
-			List<SARCHashTable.SAHTEntry> list = new List<SARCHashTable.SAHTEntry>();
-			foreach (var v in HashTable.Entries)
-			{
-				if (HashTable.GetEntryByHash(v.Hash) != v) continue;
-				list.Add(v);
-			}
-			HashTable.Entries = list;
+            if (result == DialogResult.OK)
+            {
+                HashTable.Entries.RemoveAt(listView1.SelectedIndices[0]);
+                UpdateEntries();
+            }
+        }
 
-			HashTable.Entries.Sort((v1, v2) => v1.Name.CompareTo(v2.Name));
-			UpdateEntries();
-		}
-	}
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count == 0)
+            {
+                MessageBox.Show("Please select an Entry to Rename first", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Information);return;
+            }
+            int selectedIndex = listView1.SelectedIndices[0];
+            SARCHashTable.SAHTEntry selectedEntry = HashTable.Entries[selectedIndex];
+            string newName = Microsoft.VisualBasic.Interaction.InputBox(
+                "Please give the new name or path of the file:",
+                "Rename Entry",
+                selectedEntry.Name
+            );
+
+            if (string.IsNullOrWhiteSpace(newName))
+            {
+                return;
+            }
+
+
+            var existingEntry = HashTable.GetEntryByName(newName);
+            if (existingEntry != null && existingEntry != selectedEntry)
+            {
+                MessageBox.Show($"The name '{newName}' already exists!", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            selectedEntry.Name = newName;
+            selectedEntry.Hash = SARC.GetHashFromName(newName, 0x65);
+            UpdateEntries();
+            if (selectedIndex < listView1.Items.Count)
+            {
+                listView1.Items[selectedIndex].Selected = true;
+                listView1.Items[selectedIndex].Focused = true;
+            }
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            ImportFromText();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            ExportToText();
+        }
+
+        private void ExportToText()
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                sfd.Title = "Export Hash Table Entries";
+                sfd.FileName = "HashTable";
+                sfd.DefaultExt = "txt";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamWriter writer = new StreamWriter(sfd.FileName))
+                        {
+                            foreach (var entry in HashTable.Entries)
+                            {
+                                writer.WriteLine(entry.Name);
+                            }
+                        }
+                        MessageBox.Show($"Successfully exported {HashTable.Entries.Count} entries!", "Export Complete",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error exporting entries: {ex.Message}", "Export Error",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void ImportFromText()
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                ofd.Title = "Import Hash Table Entries";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var existingNames = new HashSet<string>(HashTable.Entries.Select(e => e.Name));
+                        var newEntries = new List<SARCHashTable.SAHTEntry>();
+                        int addedCount = 0;
+                        int duplicateCount = 0;
+
+                        using (StreamReader reader = new StreamReader(ofd.FileName))
+                        {
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                line = line.Trim();
+                                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                                if (!existingNames.Contains(line))
+                                {
+                                    newEntries.Add(new SARCHashTable.SAHTEntry(line));
+                                    existingNames.Add(line);
+                                    addedCount++;
+                                }
+                                else
+                                {
+                                    duplicateCount++;
+                                }
+                            }
+                        }
+
+                        HashTable.Entries.AddRange(newEntries);
+                        UpdateEntries();
+
+                        string message = $"Imported {addedCount} new entries";
+                        if (duplicateCount > 0)
+                        {
+                            message += $"\nSkipped {duplicateCount} duplicates";
+                        }
+
+                        MessageBox.Show(message, "Import Complete",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error importing entries: {ex.Message}", "Import Error",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (isAscending)
+            {
+                HashTable.Entries.Sort((v1, v2) => v1.Name.CompareTo(v2.Name));
+                toolStripButton3.Text = "Sort Z-A";
+            }
+            else
+            {
+                HashTable.Entries.Sort((v1, v2) => v2.Name.CompareTo(v1.Name));
+                toolStripButton3.Text = "Sort A-Z";
+            }
+
+            isAscending = !isAscending;
+            UpdateEntries();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            RemoveDuplicates();
+        }
+
+        private void RemoveDuplicates()
+        {
+            DialogResult result = MessageBox.Show(
+                "This will remove all entries with duplicate hash values. Continue?",
+                "Remove duplicates",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+            );
+
+            if (result != DialogResult.Yes) return;
+
+            var uniqueEntries = new Dictionary<uint, SARCHashTable.SAHTEntry>();
+            var duplicates = new List<SARCHashTable.SAHTEntry>();
+
+            foreach (var entry in HashTable.Entries)
+            {
+                if (!uniqueEntries.ContainsKey(entry.Hash))
+                {
+                    uniqueEntries.Add(entry.Hash, entry);
+                }
+                else
+                {
+                    duplicates.Add(entry);
+                }
+            }
+
+            foreach (var dup in duplicates)
+            {
+                HashTable.Entries.Remove(dup);
+            }
+
+            UpdateEntries();
+            toolStripButton2.Enabled = false;
+            toolStripButton4.Enabled = false;
+            MessageBox.Show($"Removed {duplicates.Count} duplicate entries", "Cleanup completed");
+        }
+    }
 }
