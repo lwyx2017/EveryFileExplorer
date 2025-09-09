8334420ae14f1fbb66ed5f5e142d86dc63457260
a710010028 <710010028@qq.com>
2025-09-09 22:46:33 +0800

    Temporary annotation code

--- Switch/BNTX.cs
+++ Switch/BNTX.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using LibEveryFileExplorer.Files;
+using LibEveryFileExplorer.IO;
+using System;
 using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
-using LibEveryFileExplorer.Files;
-using LibEveryFileExplorer.IO;
 
 namespace Switch
 {
-    public class BNTX:FileFormat<BNTX.BNTXIdentifier>,IViewable
+    public class BNTX : FileFormat<BNTX.BNTXIdentifier>//, IViewable
     {
         public BNTX(byte[] Data)
         {
@@ -35,7 +35,7 @@ namespace Switch
         {
             public BNTXHeader(EndianBinaryReaderEx er)
             {
-                Signature = er.ReadString(Encoding.ASCII,4);
+                Signature = er.ReadString(Encoding.ASCII, 4);
                 if (Signature != "BNTX") throw new SignatureNotCorrectException(Signature, "BNTX", er.BaseStream.Position - 4);
                 Padding = er.ReadUInt32();
                 DataLength = er.ReadUInt32();
@@ -76,6 +76,82 @@ namespace Switch
             public UInt64 DataBlockOffset;
             public UInt64 NameDictionaryOffset;
             public UInt32 NameDictionaryLength;
+        }
+
+        public BRTIInfo BRTI;
+        public class BRTIInfo
+        {
+            public BRTIInfo(EndianBinaryReaderEx er)
+            {
+                Signature = er.ReadString(Encoding.ASCII, 4);
+                if (Signature != "BRTI") throw new SignatureNotCorrectException(Signature, "BRTI", er.BaseStream.Position - 4);
+                BRTILength0 = er.ReadUInt32();
+                BRTILength1 = er.ReadUInt64();
+                Flags = er.ReadByte();
+                Dimensions = er.ReadByte();
+                TileMode = er.ReadUInt16();
+                SwizzleSize = er.ReadUInt16();
+                MipmapCount = er.ReadUInt16();
+                MultiSampleCount = er.ReadUInt16();
+                Reversed1A = er.ReadUInt16();
+                Format = er.ReadUInt32();
+                AccessFlags = er.ReadUInt32();
+                Width = er.ReadUInt32();
+                Height = er.ReadUInt32();
+                Depth = er.ReadUInt32();
+                ArrayCount = er.ReadUInt32();
+                BlockHeightLog2 = er.ReadUInt32();
+                Reserved38 = er.ReadUInt32();
+                Reserved3C = er.ReadUInt32();
+                Reserved40 = er.ReadUInt32();
+                Reserved44 = er.ReadUInt32();
+                Reserved48 = er.ReadUInt32();
+                Reserved4C = er.ReadUInt32();
+                DataLength = er.ReadUInt32();
+                Alignment = er.ReadUInt32();
+                ChannelTypes = er.ReadUInt32();
+                TextureType = er.ReadUInt32();
+                NameOffset = er.ReadUInt64();
+                ParentOffset = er.ReadUInt64();
+                PtrsOffset = er.ReadUInt64();
+            }
+            public String Signature;
+            public UInt32 BRTILength0;
+            public UInt64 BRTILength1;
+            public Byte Flags;
+            public Byte Dimensions;
+            public UInt16 TileMode;
+            public UInt16 SwizzleSize;
+            public UInt16 MipmapCount;
+            public UInt16 MultiSampleCount;
+            public UInt16 Reversed1A;
+            public UInt32 Format;
+            public UInt32 AccessFlags;
+            public UInt32 Width;
+            public UInt32 Height;
+            public UInt32 Depth;
+            public UInt32 ArrayCount;
+            public UInt32 BlockHeightLog2;
+            public UInt32 Reserved38;
+            public UInt32 Reserved3C;
+            public UInt32 Reserved40;
+            public UInt32 Reserved44;
+            public UInt32 Reserved48;
+            public UInt32 Reserved4C;
+            public UInt32 DataLength;
+            public UInt32 Alignment;
+            public UInt32 ChannelTypes;
+            public UInt32 TextureType;
+            public UInt64 NameOffset;
+            public UInt64 ParentOffset;
+            public UInt64 PtrsOffset;
+            public UInt64 PtrsSize;
+        }
+
+        public BNTXTextures Textures;
+        public class BNTXTextures
+        {
+
         }
 
         public class BNTXIdentifier:FileFormatIdentifier
