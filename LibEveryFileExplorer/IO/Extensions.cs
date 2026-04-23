eb4ae0aa8352e33f62a8b37b35f565c2d7391822
a710010028 <710010028@qq.com>
2026-04-24 01:58:43 +0800

    Submit some things

---
+++ LibEveryFileExplorer/IO/Extensions.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using OpenTK;
+
+namespace LibEveryFileExplorer.IO
+{
+    public static class Extensions
+    {
+        public static Dictionary<string, long> Markers = new Dictionary<string, long>();
+
+        public static void SetMarkerOnCurrentOffset(this EndianBinaryReader er, string name)
+        {
+            Markers.Add(name, er.BaseStream.Position);
+        }
+
+        public static void SetMarker(this EndianBinaryReader er, string name, long offset)
+        {
+            Markers.Add(name, offset);
+        }
+
+        public static long GetMarker(this EndianBinaryReader er, string name)
+        {
+            return Markers[name];
+        }
+
+        public static void RemoveMarker(this EndianBinaryReader er, string name)
+        {
+            Markers.Remove(name);
+        }
+
+        public static void ClearMarkers(this EndianBinaryReader er)
+        {
+            Markers.Clear();
+        }
+
+        public static float ReadSingleInt32Exp12(this EndianBinaryReader er)
+        {
+            return (float)er.ReadInt32() / 4096;
+        }
+
+        public static float[] ReadSingleInt32Exp12s(this EndianBinaryReader er, int count)
+        {
+            float[] values = new float[count];
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = er.ReadSingleInt32Exp12();
+            }
+            return values;
+        }
+
+        public static float ReadSingleInt16Exp12(this EndianBinaryReader er)
+        {
+            return (float)er.ReadInt16() / 4096;
+        }
+
+        public static float[] ReadSingleInt16Exp12s(this EndianBinaryReader er, int count)
+        {
+            float[] values = new float[count];
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = er.ReadSingleInt16Exp12();
+            }
+            return values;
+        }
+
+        public static int ReadVariableLength(this EndianBinaryReader er)
+        {
+            byte b = er.ReadByte();
+            int value = b & 0x7F;
+            while (er.BaseStream.Position < er.BaseStream.Length && (b & 0x80) != 0)
+            {
+                value <<= 7;
+                b = er.ReadByte();
+                value |= b & 0x7F;
+            }
+            return value;
+        }
+
+        public static Color ReadColor4Singles(this EndianBinaryReader er)
+        {
+            int red = (int)(er.ReadSingle() * 255);
+            int green = (int)(er.ReadSingle() * 255);
+            int blue = (int)(er.ReadSingle() * 255);
+            int alpha = (int)(er.ReadSingle() * 255);
+            return Color.FromArgb(alpha, red, green, blue);
+        }
+
+        public static Color ReadColor16(this EndianBinaryReader er)
+        {
+            short r = er.ReadInt16();
+            short g = er.ReadInt16();
+            short b = er.ReadInt16();
+            short a = er.ReadInt16();
+            return Color.FromArgb(System.Math.Abs(a) & 0xFF, System.Math.Abs(r) & 0xFF, System.Math.Abs(g) & 0xFF, System.Math.Abs(b) & 0xFF);
+        }
+
+        public static Color ReadColor8(this EndianBinaryReader er)
+        {
+            int red = er.ReadByte();
+            int green = er.ReadByte();
+            int blue = er.ReadByte();
+            int alpha = er.ReadByte();
+            return Color.FromArgb(alpha, red, green, blue);
+        }
+
+        private static int smfGetVarLengthSize(int value)
+        {
+            int size = 1;
+            int temp = value;
+            while (temp > 127 && size < 4)
+            {
+                size++;
+                temp >>= 7;
+            }
+            return size;
+        }
+
+        public static unsafe long IndexOf(this byte[] haystack, byte[] needle)
+        {
+            fixed (byte* hPtr = haystack)
+            fixed (byte* nPtr = needle)
+            {
+                long index = 0;
+                byte* hCurrent = hPtr;
+                byte* hEnd = hPtr + haystack.LongLength;
+
+                for (; hCurrent < hEnd; hCurrent++)
+                {
+                    bool match = true;
+                    byte* hTemp = hCurrent;
+                    byte* nTemp = nPtr;
+                    byte* nEnd = nPtr + needle.LongLength;
+
+                    while (match && nTemp < nEnd)
+                    {
+                        match = *nTemp == *hTemp;
+                        nTemp++;
+                        hTemp++;
+                    }
+
+                    if (match)
+                        return index;
+
+                    index++;
+                }
+
+                return -1;
+            }
+        }
+
+        public static unsafe List<long> IndexesOf(this byte[] haystack, byte[] needle)
+        {
+            List<long> indexes = new List<long>();
+
+            fixed (byte* hPtr = haystack)
+            fixed (byte* nPtr = needle)
+            {
+                long index = 0;
+                byte* hCurrent = hPtr;
+                byte* hEnd = hPtr + haystack.LongLength;
+
+                for (; hCurrent < hEnd; hCurrent++)
+                {
+                    bool match = true;
+                    byte* hTemp = hCurrent;
+                    byte* nTemp = nPtr;
+                    byte* nEnd = nPtr + needle.LongLength;
+
+                    while (match && nTemp < nEnd)
+                    {
+                        match = *nTemp == *hTemp;
+                        nTemp++;
+                        hTemp++;
+                    }
+
+                    if (match)
+                        indexes.Add(index);
+
+                    index++;
+                }
+            }
+
+            return indexes;
+        }
+
+        public static Vector3 ToVector3(this Color c)
+        {
+            return new Vector3((float)c.R / 255, (float)c.G / 255, (float)c.B / 255);
+        }
+    }
+}
---
+++ NDS/NitroSystem/G3D/MTX44.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using OpenTK;
+
+namespace NDS.NitroSystem.G3D
+{
+    public class MTX44
+    {
+        private const int MatrixSize = 4;
+        private const int MatrixElementCount = 16;
+        private float[] _matrixArray = new float[MatrixElementCount];
+
+        public float this[int x, int y]
+        {
+            get => _matrixArray[x + y * MatrixSize];
+            set => _matrixArray[x + y * MatrixSize] = value;
+        }
+
+        public float this[int index]
+        {
+            get => _matrixArray[index];
+            set => _matrixArray[index] = value;
+        }
+
+        public float[] Floats => _matrixArray;
+
+        public MTX44()
+        {
+            LoadIdentity();
+        }
+
+        public void SetValues(float[] array)
+        {
+            _matrixArray = array;
+        }
+
+        public MTX44 Clone()
+        {
+            MTX44 newMatrix = new MTX44();
+            for (int i = 0; i < MatrixElementCount; i++)
+            {
+                newMatrix._matrixArray[i] = _matrixArray[i];
+            }
+            return newMatrix;
+        }
+
+        public void LoadIdentity()
+        {
+            Zero();
+            this[0, 0] = 1;
+            this[1, 1] = 1;
+            this[2, 2] = 1;
+            this[3, 3] = 1;
+        }
+
+        public void Zero()
+        {
+            Array.Clear(_matrixArray, 0, _matrixArray.Length);
+        }
+
+        public void CopyValuesTo(MTX44 targetMatrix)
+        {
+            for (int i = 0; i < MatrixElementCount; i++)
+            {
+                targetMatrix._matrixArray[i] = this[i];
+            }
+        }
+
+        public void Translate(float x, float y, float z)
+        {
+            MTX44 translationMatrix = new MTX44();
+            translationMatrix.LoadIdentity();
+            translationMatrix[12] = x;
+            translationMatrix[13] = y;
+            translationMatrix[14] = z;
+            MultMatrix(translationMatrix).CopyValuesTo(this);
+        }
+
+        public void Rotate(float x, float y, float z)
+        {
+            MTX44 rotationMatrix = new MTX44();
+            rotationMatrix.LoadIdentity();
+            float cosX = (float)Math.Cos(x);
+            float sinX = (float)Math.Sin(x);
+            float cosY = (float)Math.Cos(y);
+            float sinY = (float)Math.Sin(y);
+            float cosZ = (float)Math.Cos(z);
+            float sinZ = (float)Math.Sin(z);
+            rotationMatrix[0] = cosY * cosZ;
+            rotationMatrix[1] = cosY * sinZ;
+            rotationMatrix[2] = -sinY;
+            rotationMatrix[4] = cosZ * sinX * sinY - sinZ * cosX;
+            rotationMatrix[5] = sinX * sinY + cosX * cosZ;
+            rotationMatrix[6] = sinX * cosY;
+            rotationMatrix[8] = cosX * cosZ * sinY + sinX * sinZ;
+            rotationMatrix[9] = cosX * sinY * sinZ - sinX * cosZ;
+            rotationMatrix[10] = cosX * cosY;
+            MultMatrix(rotationMatrix).CopyValuesTo(this);
+        }
+
+        public void Scale(float x, float y, float z)
+        {
+            MTX44 scaleMatrix = new MTX44();
+            scaleMatrix.LoadIdentity();
+            scaleMatrix[0] = x;
+            scaleMatrix[5] = y;
+            scaleMatrix[10] = z;
+            MultMatrix(scaleMatrix).CopyValuesTo(this);
+        }
+
+        public MTX44 MultMatrix(MTX44 matrixB)
+        {
+            MTX44 resultMatrix = new MTX44();
+
+            for (int row = 0; row < MatrixSize; row++)
+            {
+                for (int col = 0; col < MatrixSize; col++)
+                {
+                    resultMatrix._matrixArray[(row << 2) + col] = 0;
+
+                    for (int k = 0; k < MatrixSize; k++)
+                    {
+                        resultMatrix._matrixArray[(row << 2) + col] +=
+                            _matrixArray[(k << 2) + col] * matrixB._matrixArray[(row << 2) + k];
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public Vector3 MultVector(Vector3 vector)
+        {
+            float[] floatVector = new[] { vector.X, vector.Y, vector.Z };
+            float[] result = MultVector(floatVector);
+            return new Vector3(result[0], result[1], result[2]);
+        }
+
+        public float[] MultVector(float[] vector)
+        {
+            float[] result = new float[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                float xPart = vector[0] * this[i];
+                float yPart = vector[1] * this[4 + i];
+                float zPart = vector[2] * this[8 + i];
+                float translatePart = this[12 + i];
+
+                result[i] = xPart + yPart + zPart + translatePart;
+            }
+
+            return result;
+        }
+
+        public static MTX44 mtx_Rotate(int pivot, int neg, float a, float b)
+        {
+            float[] matrixArray = new float[MatrixElementCount]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 1
+            };
+
+            float sign = 1;
+            float paramA = a;
+            float paramB = b;
+            switch (neg)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 9:
+                case 11:
+                case 13:
+                case 15:
+                    sign = -1;
+                    break;
+            }
+            switch (neg)
+            {
+                case 2:
+                case 3:
+                case 6:
+                case 7:
+                case 10:
+                case 11:
+                case 14:
+                case 15:
+                    paramB = -paramB;
+                    break;
+            }
+            switch (neg)
+            {
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                    paramA = -paramA;
+                    break;
+            }
+            switch (pivot)
+            {
+                case 0:
+                    matrixArray[0] = sign;
+                    matrixArray[5] = paramA;
+                    matrixArray[6] = paramB;
+                    matrixArray[9] = paramB;
+                    matrixArray[10] = paramA;
+                    break;
+                case 1:
+                    matrixArray[1] = sign;
+                    matrixArray[4] = paramA;
+                    matrixArray[6] = paramB;
+                    matrixArray[8] = paramB;
+                    matrixArray[10] = paramA;
+                    break;
+                case 2:
+                    matrixArray[2] = sign;
+                    matrixArray[4] = paramA;
+                    matrixArray[5] = paramB;
+                    matrixArray[8] = paramB;
+                    matrixArray[9] = paramA;
+                    break;
+                case 3:
+                    matrixArray[4] = sign;
+                    matrixArray[1] = paramA;
+                    matrixArray[2] = paramB;
+                    matrixArray[9] = paramB;
+                    matrixArray[10] = paramA;
+                    break;
+                case 4:
+                    matrixArray[5] = sign;
+                    matrixArray[0] = paramA;
+                    matrixArray[2] = paramB;
+                    matrixArray[8] = paramB;
+                    matrixArray[10] = paramA;
+                    break;
+                case 5:
+                    matrixArray[6] = sign;
+                    matrixArray[0] = paramA;
+                    matrixArray[1] = paramB;
+                    matrixArray[8] = paramB;
+                    matrixArray[9] = paramA;
+                    break;
+                case 6:
+                    matrixArray[8] = sign;
+                    matrixArray[1] = paramA;
+                    matrixArray[2] = paramB;
+                    matrixArray[5] = paramB;
+                    matrixArray[6] = paramA;
+                    break;
+                case 7:
+                    matrixArray[9] = sign;
+                    matrixArray[0] = paramA;
+                    matrixArray[2] = paramB;
+                    matrixArray[4] = paramB;
+                    matrixArray[6] = paramA;
+                    break;
+                case 8:
+                    matrixArray[10] = sign;
+                    matrixArray[0] = paramA;
+                    matrixArray[1] = paramB;
+                    matrixArray[4] = paramB;
+                    matrixArray[5] = paramA;
+                    break;
+                case 9:
+                    matrixArray[0] = -paramA;
+                    break;
+            }
+
+            MTX44 resultMatrix = new MTX44();
+            resultMatrix._matrixArray = matrixArray;
+            return resultMatrix;
+        }
+
+        public static MTX44 operator +(MTX44 a, MTX44 b)
+        {
+            MTX44 result = a.Clone();
+            for (int i = 0; i < MatrixElementCount; i++)
+                result[i] += b[i];
+            return result;
+        }
+
+        public static MTX44 operator -(MTX44 a, MTX44 b)
+        {
+            MTX44 result = a.Clone();
+            for (int i = 0; i < MatrixElementCount; i++)
+                result[i] -= b[i];
+            return result;
+        }
+
+        public static MTX44 operator *(MTX44 a, float b)
+        {
+            MTX44 result = a.Clone();
+            for (int i = 0; i < MatrixElementCount; i++)
+                result[i] *= b;
+            return result;
+        }
+
+        public static MTX44 operator *(float b, MTX44 a)
+        {
+            return a * b;
+        }
+
+        public static MTX44 operator /(float b, MTX44 a)
+        {
+            MTX44 result = a.Clone();
+            for (int i = 0; i < MatrixElementCount; i++)
+                result[i] = b / result[i];
+            return result;
+        }
+
+        public static implicit operator float[](MTX44 matrix)
+        {
+            return matrix.Floats;
+        }
+
+        public static implicit operator MTX44(float[] array)
+        {
+            MTX44 matrix = new MTX44();
+            matrix._matrixArray = array;
+            return matrix;
+        }
+    }
+}
